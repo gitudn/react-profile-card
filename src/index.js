1fb8cfcdcf1e12bed5a9d55a2fb1b5813a50ed71
gitudn <hossain.udn@gmail.com>
2024-05-22 16:49:34 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./style.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="hossainUddin.jpeg" alt="avatar image" />;
+}
+function Intro() {
+  return (
+    <div>
+      <h1>Hossain Uddin</h1>
+      <p style={{ fontSize: "11px" }}>
+        I'm a web developer skilled in HTML, CSS, and JavaScript. I create
+        responsive and user-friendly websites. Let's bring your ideas to life
+        online
+      </p>
+    </div>
+  );
+}
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="💪" color="green" />
+      <Skill skill="Javascript" emoji="💪" color="#3498DB  " />
+      <Skill skill="Expressjs" emoji="👦" color="#73C6B6 " />
+      <Skill skill="Html+Css" emoji="💪" color="#839192 " />
+      <Skill skill="Nodejs" emoji="👍" color="#3498DB " />
+    </div>
+  );
+}
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
